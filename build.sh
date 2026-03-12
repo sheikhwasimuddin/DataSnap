e60e6dd7254e1f43afcdc8a87dfd228a6e46cb69
sheikhwasimuddin <sheikhwasimuddin786@gmail.com>
2026-03-12 21:56:44 +0530

    pin setuptools<75 to keep pkg_resources for gcloud/pyrebase4

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Use python -m pip to guarantee we install into the active venv
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
-# Force setuptools into the venv AFTER other deps (pip may skip it otherwise)
-python -m pip install --ignore-installed setuptools
-
-# Verify pkg_resources is importable
+# Verify pkg_resources is importable (gcloud requires it)
 python -c "import pkg_resources; print('pkg_resources OK')"
